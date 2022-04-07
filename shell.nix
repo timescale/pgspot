29800d20c8220c82482f22dee2a6e0216aa87d4e
James Guthrie <jguthrie@timescale.com>
2022-04-07 13:18:14 +0200

    Add nix shell

---
+++ shell.nix
@@ -0,0 +1,38 @@
+let
+  # pin nixpkgs to a specific version, so that we're not relying on the system version
+  pkgs = import (builtins.fetchGit {
+    name = "nixpkgs-21.11";
+    url = "https://github.com/nixos/nixpkgs/";
+    # `git ls-remote https://github.com/nixos/nixpkgs nixos-unstable`
+    ref = "refs/tags/21.11";
+    rev = "506445d88e183bce80e47fc612c710eb592045ed";
+  }) {};
+
+  # Use svenklemm's fork of pglast, which adds support for SET, COMMIT, ROLLBACK, CALL
+  pglast = pkgs.python310Packages.buildPythonPackage rec {
+    name = "pglast";
+    version = "d6f6d035fb596f26b014ea1c38b9e57ac8f8a3ff";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "svenklemm";
+      repo = "${name}";
+      rev = "${version}";
+      fetchSubmodules = true;
+      sha256 = "sha256-7vZjpuK2XCBRHmkHrCXjnFQh4fvvHFtyyF+GZBeUJ4M=";
+    };
+  };
+
+  py310 = pkgs.python310;
+  py-with-packages = py310.withPackages (p: with p; [
+    pglast
+  ]);
+
+in
+pkgs.mkShell {
+  buildInputs = [
+    py-with-packages
+  ];
+  shellHook = ''
+    PYTHONPATH=${py-with-packages}/${py-with-packages.sitePackages}
+  '';
+}

cf6e87f93edfe2acc9ed8b38054e394069c68e09
James Guthrie <jguthrie@timescale.com>
2022-04-07 13:17:45 +0200

    Infrastructure for error and warning description

---
+++ codes.py
@@ -0,0 +1,104 @@
+codes = {
+    'PS001': {
+        'title': "Unqualified operator",
+        'description': """
+        TODO
+        """
+    },
+    'PS002': {
+        'title': "Unsafe function creation",
+        'description': """
+        TODO
+        """
+    },
+    'PS003': {
+        'title': "SECURITY DEFINER function without explicit search_path",
+        'description': """
+        TODO
+        """
+    },
+    'PS004': {
+        'title': "SECURITY DEFINER function with insecure search_path",
+        'description': """
+        TODO
+        """
+    },
+    'PS005': {
+        'title': "Function without explicit search_path",
+        'description': """
+        TODO
+        """
+    },
+    'PS006': {
+        'title': "Unsafe transform creation",
+        'description': """
+        TODO
+        """
+    },
+    'PS007': {
+        'title': "Unsafe object creation",
+        'description': """
+        TODO
+        """
+    },
+    'PS008': {
+        'title': "Unqualified alter sequence",
+        'description': """
+        TODO
+        """
+    },
+    'PS009': {
+        'title': "Unsafe CASE expression",
+        'description': """
+        TODO
+        """
+    },
+    'PS010': {
+        'title': "Unsafe schema creation",
+        'description': """
+        TODO
+        """
+    },
+    'PS011': {
+        'title': "Unsafe sequence creation",
+        'description': """
+        TODO
+        """
+    },
+    'PS012': {
+        'title': "Unsafe table creation",
+        'description': """
+        TODO
+        """
+    },
+    'PS013': {
+        'title': "Unsafe foreign server creation",
+        'description': """
+        TODO
+        """
+    },
+    'PS014': {
+        'title': "Unsafe index creation",
+        'description': """
+        TODO
+        """
+    },
+    'PS015': {
+        'title': "Unsafe view creation",
+        'description': """
+        TODO
+        """
+    },
+    'PS016': {
+        'title': "Unqualified function call",
+        'description': """
+        TODO
+        """
+    },
+    'PS017': {
+        'title': "Unqualified object reference",
+        'description': """
+        TODO
+        """
+    },
+}

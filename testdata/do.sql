30a45bfd160b674aaf6e47ab95c285bbb0aabc3c
Sven Klemm <sven@timescale.com>
2022-04-14 13:27:53 +0200

    Add support for nested DO statements
    Create the dollar quoting wrapper for the DO statement dynamically
    so we can support nested DO blocks.

--- testdata/do.sql
+++ testdata/do.sql
@@ -2,3 +2,22 @@
 DO LANGUAGE PLPGSQL $$ BEGIN SELECT unsafe_call2(); END; $$;
 DO LANGUAGE PLPGSQL $$ BEGIN PERFORM unsafe_call3(); END; $$;
 
+-- nested DO
+DO LANGUAGE PLPGSQL $$
+BEGIN
+  PERFORM unsafe_call8();
+  DO $i$
+  BEGIN
+    PERFORM unsafe_call10();
+  END;
+  $i$;
+  SET search_path TO pg_catalog;
+  DO $i$
+  BEGIN
+    PERFORM safe_call17();
+    RESET search_path;
+    PERFORM unsafe_call19();
+  END;
+  $i$;
+END;
+$$;

cdead363096b1c7f1d4f15552593deae0bfb269b
James Guthrie <JamesGuthrie@users.noreply.github.com>
2022-04-20 08:51:28 +0200

    'Unqualified object reference' for CREATE (AGGREGATE|OPERATOR|TYPE) (#25)

---
+++ tests/create_aggregate_test.py
@@ -0,0 +1,27 @@
+import subprocess
+import shlex
+
+
+def test_create_old_style_aggregate():
+    sql = """
+    CREATE AGGREGATE aggregate(SFUNC=agg_sfunc,STYPE=internal);
+    """
+    result = subprocess.run(["echo {} | python pgspot".format(shlex.quote(sql))], shell=True, capture_output=True, text=True)
+    output = result.stdout
+
+    assert "PS017" in output
+
+
+def test_create_new_style_aggregate():
+    sql = """
+    CREATE AGGREGATE sum (complex)
+    (
+        sfunc = complex_add,
+        stype = complex,
+        initcond = '(0,0)'
+    );
+    """
+    result = subprocess.run(["echo {} | python pgspot".format(shlex.quote(sql))], shell=True, capture_output=True, text=True)
+    output = result.stdout
+
+    assert "PS017" in output

63fc6c6a32200101fc18c022e08b33097619df4b
Sven Klemm <sven@timescale.com>
2024-11-03 22:12:28 +0100

    Add more format string tests

--- tests/format_string_test.py
+++ tests/format_string_test.py
@@ -1,14 +1,35 @@
-
 from pgspot.pg_catalog.format import parse_format_string
+
+# %[position][flags][width]type
 
 
 def test_no_variables():
-    ret = parse_format_string("")
-    assert ret == []
+    assert parse_format_string("") == []
+    assert parse_format_string("SELECT * FROM table") == []
+    assert parse_format_string("%%") == []
 
-    ret = parse_format_string("SELECT * FROM table")
-    assert ret == []
 
-    ret = parse_format_string("%%")
-    assert ret == []
+def test_single_variable():
+    assert parse_format_string("%s") == [("s", 1)]
+    assert parse_format_string("%I") == [("I", 1)]
+    assert parse_format_string("%L") == [("L", 1)]
 
+    assert parse_format_string("%%%s%%") == [("s", 1)]
+
+    assert parse_format_string("%1$s") == [("s", 1)]
+    assert parse_format_string("%3$s") == [("s", 3)]
+
+    assert parse_format_string("%-s") == [("s", 1)]
+    assert parse_format_string("%-10s") == [("s", 1)]
+    assert parse_format_string("%-*s") == [("s", 2)]
+    assert parse_format_string("%-*1$s") == [("s", 1)]
+
+    assert parse_format_string("%7$-s") == [("s", 7)]
+    assert parse_format_string("%7$-10s") == [("s", 7)]
+    assert parse_format_string("%7$-*s") == [("s", 7)]
+    assert parse_format_string("%7$-*1$s") == [("s", 7)]
+
+
+def test_multiple_variable():
+    assert parse_format_string("%s%I%L") == [("s", 1), ("I", 2), ("L", 3)]
+    assert parse_format_string("%3$s%2$I%1$L") == [("s", 3), ("I", 2), ("L", 1)]

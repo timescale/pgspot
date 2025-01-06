3185d96366cb9b4343a093fde34975061b08d236
Sven Klemm <sven@timescale.com>
2025-01-06 20:57:14 +0100

    Add additional RETURN QUERY EXECUTE test with USING

--- testdata/return_query.sql
+++ testdata/return_query.sql
@@ -3,10 +3,12 @@ create function ret_query() returns table(y int)
 as $func$
 declare
   a text;
+  b text;
 begin
     return query execute $$select format('abc',1,2)::int$$;
-    return query execute a;
-    return query execute a + 'def';
+    return query execute $$select format('abc',$1,2)::int$$ using a;
+    return query execute b;
+    return query execute b || b;
 end;
 $func$ language plpgsql;
 
